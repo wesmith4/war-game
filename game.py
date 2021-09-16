9e06d3caa75ad9675fff582b9ef27497117b02c1
Will Smith <william.e.smithiv@gmail.com>
2021-09-16 16:05:07 -0600

    bug fixing

--- game.py
+++ game.py
@@ -1,58 +1,8 @@
-from collections import deque
-from src.objects import Game, Deck, Hand, Card
-
-ranks = {
-    "A": 13,
-    "2": 1,
-    "3": 2,
-    "4": 3,
-    "5": 4,
-    "6": 5,
-    "7": 6,
-    "8": 7,
-    "9": 8,
-    "10": 9,
-    "J": 10,
-    "Q": 11,
-    "K": 12,
-}
+from src.objects import Game
+import streamlit as st
 
 
-def playTurn(game: Game):
-    stack = deque()
-    card1 = game.hand1.draw()
-    card2 = game.hand2.draw()
+game = Game("P1", "P2")
+results = game.play()
 
-    if card1.value == card2.value:
-        while card1.value == card2.value:
-            stack.appendleft(card1)
-            stack.appendleft(card2)
-            stack.appendleft(game.hand1.draw())
-            stack.appendleft(game.hand2.draw())
-
-            card1 = game.hand1.draw()
-            card2 = game.hand2.draw()
-
-    stack.appendleft(card1)
-    stack.appendleft(card2)
-
-    if card1Higher(card1, card2):
-        game.hand1.pick_up_cards(stack)
-    else:
-        game.hand2.pick_up_cards(stack)
-
-
-def card1Higher(card1: Card, card2: Card):
-    """Returns True or False"""
-    return ranks[card1.value] > ranks[card2.value]
-
-
-game = Game()
-game.deal()
-
-
-while game.in_progress():
-    playTurn(game)
-
-
-print(game.hand1.cards.__len__())
+print(results)
--- src/objects.py
+++ src/objects.py
@@ -1,5 +1,22 @@
 from collections import deque
 from random import shuffle
+import pandas as pd
+
+ranks = {
+    "A": 13,
+    "2": 1,
+    "3": 2,
+    "4": 3,
+    "5": 4,
+    "6": 5,
+    "7": 6,
+    "8": 7,
+    "9": 8,
+    "10": 9,
+    "J": 10,
+    "Q": 11,
+    "K": 12,
+}
 
 
 class Card:
@@ -50,13 +67,21 @@ class Hand:
         self.cards.extendleft(cards)
 
 
-class Game:
-    def __init__(self):
-        self.deck = Deck()
-        self.hand1 = Hand()
-        self.hand2 = Hand()
+class Player:
+    def __init__(self, name: str, hand: Hand):
+        self.name = name
+        self.hand = hand
 
-    def deal(self):
+
+class Game:
+    def __init__(self, player1Name, player2Name):
+        self.deck = Deck()
+        self.player1 = Player(player1Name, Hand())
+        self.player2 = Player(player2Name, Hand())
+        self.turn = 0
+        self.history = []
+
+    def shuffle_and_deal(self):
         self.deck.shuffle()
 
         hand1 = True
@@ -64,13 +89,101 @@ class Game:
             card = self.deck.take_one()
 
             if hand1:
-                self.hand1.pick_up_cards(deque([card]))
+                self.player1.hand.pick_up_cards(deque([card]))
             else:
-                self.hand2.pick_up_cards(deque([card]))
+                self.player2.hand.pick_up_cards(deque([card]))
 
             hand1 = not hand1
 
-        print("Finished dealing")
+        self.history.append(
+            {
+                "Turn": self.turn,
+                "P1": self.player1.hand.get_length(),
+                "P2": self.player2.hand.get_length(),
+            }
+        )
+
+    def playTurn(self):
+        self.turn += 1
+
+        stack = deque()
+        card1 = self.player1.hand.draw()
+        card2 = self.player2.hand.draw()
+
+        if card1.value == card2.value:
+            if self.player1.hand.get_length() < 2:
+                stack.appendleft(card1)
+                stack.appendleft(card2)
+                self.player2.hand.pick_up_cards(stack)
+            elif self.player2.hand.get_length() < 2:
+                stack.appendleft(card1)
+                stack.appendleft(card2)
+                self.player1.hand.pick_up_cards(stack)
+
+            while card1.value == card2.value:
+                stack.appendleft(card1)
+                stack.appendleft(card2)
+                stack.appendleft(self.player1.hand.draw())
+                stack.appendleft(self.player2.hand.draw())
+
+                card1 = self.player1.hand.draw()
+                card2 = self.player2.hand.draw()
+
+        stack.appendleft(card1)
+        stack.appendleft(card2)
+
+        if ranks[card1.value] > ranks[card2.value]:
+            self.player1.hand.pick_up_cards(stack)
+        else:
+            self.player2.hand.pick_up_cards(stack)
+
+        self.history.append(
+            {
+                "Turn": self.turn,
+                "P1": self.player1.hand.get_length(),
+                "P2": self.player2.hand.get_length(),
+            }
+        )
 
     def in_progress(self):
-        return self.hand1.get_length() and self.hand2.get_length()
+        return (
+            self.player1.hand.get_length() > 0
+            and self.player2.hand.get_length() > 0
+        )
+
+    def get_results(self):
+        if self.player1.hand.get_length() == 52:
+            winner = self.player1
+        elif self.player2.hand.get_length() == 52:
+            winner = self.player2
+        else:
+            raise Exception("Game still in progress")
+
+        return {
+            "Winner": winner,
+            "Turns": self.turn,
+            "Results": pd.DataFrame(self.history),
+        }
+
+    def play(self):
+        self.shuffle_and_deal()
+        print(
+            "%s : %s cards"
+            % (self.player1.name, self.player1.hand.get_length())
+        )
+        print(
+            "%s : %s cards"
+            % (self.player2.name, self.player2.hand.get_length())
+        )
+        while self.in_progress():
+            self.playTurn()
+            print(
+                "%s : %s cards"
+                % (self.player1.name, self.player1.hand.get_length())
+            )
+            print(
+                "%s : %s cards"
+                % (self.player2.name, self.player2.hand.get_length())
+            )
+
+        return self.get_results()
